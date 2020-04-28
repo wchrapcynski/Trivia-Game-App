c1ed3ac34b250d588743f8cea1a93ad5cbf88cb9
William Chrapcynski <wchrapcynski@gmail.com>
2020-04-27 21:32:40 -0400

    Question box interface is set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Logo from './images/logo2.png'
 import './App.scss';
+import Question from './components/question/question'
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="header">
+        <img className="header__logo" src={Logo} alt="Disney Trivia Logo"/>
+        <Question />
+      </header>
     </div>
   );
 }
