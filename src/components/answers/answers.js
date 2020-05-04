57cf7aa6a791e7f73f952d0dd658d1044492222c
William Chrapcynski <wchrapcynski@gmail.com>
2020-05-04 14:09:05 -0400

    Responsiveness for mobile is updated.

--- src/components/answers/answers.js
+++ src/components/answers/answers.js
@@ -5,10 +5,10 @@ import './answers.scss';
 function Answers() {
   return(
     <div className="answers">
-      <Choice question="Est occaecat irure culpa Lorem velit ad incididunt velit deserunt."/>
-      <Choice question="Est occaecat irure culpa Lorem velit ad incididunt velit deserunt."/>
-      <Choice question="Est occaecat irure culpa Lorem velit ad incididunt velit deserunt."/>
-      <Choice question="Est occaecat irure culpa Lorem velit ad incididunt velit deserunt."/>
+      <Choice question="Est occaecat irure culpa Lorem velit ad incididunt velit."/>
+      <Choice question="Est occaecat irure culpa Lorem velit ad incididunt velit."/>
+      <Choice question="Est occaecat irure culpa Lorem velit ad incididunt velit."/>
+      <Choice question="Est occaecat irure culpa Lorem velit ad incididunt velit."/>
     </div>
   )
 }
