a1ff2800d16e6ff69850f889b3911dd17718f020
William Chrapcynski <wchrapcynski@gmail.com>
2020-05-08 19:15:29 -0400

    Started to experiment with fetching data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Logo from "./images/logo2.png";
 import "./App.scss";
 import Question from "./components/question/question";
@@ -6,6 +6,57 @@ import Answers from "./components/answers/answers";
 import Navigation from "./components/navigation/navigation";
 
 function App() {
+  const apiAccessOptions = {method: "GET", headers: {"Authorization": "Token " + process.env.REACT_APP_TRIVIA_API_KEY}}
+  const baseUrl = "http://localhost:8000";
+  const questionListUrl = baseUrl + "/trivia/";
+  const questionUrl = baseUrl + "/question/";
+  const categorysearch = baseUrl + "/categorysearch/"
+  const leaderboardUrl = baseUrl + "/leaderboard/";
+  const publishedUrl = baseUrl + "/published/";
+
+  const [questions, setQuestions] = useState(null)
+  const [leaderboard, setLeaderboard] = useState(null)
+  const [published, setPublished] = useState(null)
+
+  const fetchData = (url, setData) => {
+    fetch(url, apiAccessOptions)
+    .then(response => response.json())
+    .then(response => setData(response))
+    .catch(error => console.log("Data was not received"))
+  }
+
+  // useEffect(() => {
+  //   fetchData(questionUrl + "/3", setQuestions)
+  //   /* eslint-disable react-hooks/exhaustive-deps */
+  // }, [])
+
+  // useEffect(() => {
+  //   fetchData(publishedUrl, setPublished)
+    
+  // }, [])
+
+  useEffect(() => {
+    fetchData('http://localhost:8000/categorysearch/?category=movies', setQuestions)
+    /* eslint-disable react-hooks/exhaustive-deps */
+  }, [])
+
+  // useEffect(() => {
+  //   fetchData(leaderboardUrl, setLeaderboard)
+  //   /* eslint-disable react-hooks/exhaustive-deps */
+  // }, [])
+
+  useEffect(() => {
+    if(questions) {
+      console.log(questions)
+    }
+    if(leaderboard) {
+      console.log(leaderboard)
+    }
+    if(published) {
+      console.log(published)
+    }
+  }, [questions, leaderboard, published])
+  
   return (
     <div className="App">
       <header className="header">
--- src/components/question/question.js
+++ src/components/question/question.js
@@ -3,7 +3,7 @@ import './question.scss'
 
 function Question() {
   return <div className="question-box">
-    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa ducimus magni ad ipsa! Itaque ab aspernatur porro explicabo cupiditate commodi a sit voluptatibus!</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ducimus magni ad ipsa! Itaque ab aspernatur porro explicabo cupiditate commodi a sit voluptatibus!</div>
   </div>;
 }
 
