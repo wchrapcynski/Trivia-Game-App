b909ccab75ab377b9cad69ce755f05c6ea0558c4
William Chrapcynski <wchrapcynski@gmail.com>
2020-05-26 17:09:29 -0400

    Added right-wrong images when an answer is chosen

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,24 +1,40 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import right from "./../../images/right.svg";
+import wrong from "./../../images/wrong.svg";
 import "./question.scss";
 
 function Question() {
   const questionData = useSelector((state) => state.questionReducer.question);
-  const { hasGameStarted, hasGameEnded, highScore } = useSelector(
-    (state) => state.gameReducer
-  );
+  const {
+    hasGameStarted,
+    hasGameEnded,
+    highScore,
+    isQuestionActive,
+    isCorrect,
+  } = useSelector((state) => state.gameReducer);
   return (
     <div className="question-box">
-      {!hasGameStarted && (
-        <p className="question-box__high_score">
-          The Current High Score is: {highScore}
-        </p>
+      {!hasGameStarted ? (
+        <div>
+          <p>The Current High Score is: {highScore}</p>
+          <p>
+            "Welcome to Mouse Fan Trivia! How many questions can you answer?"
+          </p>
+        </div>
+      ) : hasGameEnded ? (
+        <p>"Game Over! Click on Start Game to try again!"</p>
+      ) : isCorrect === null ? (
+        <p>{questionData}</p>
+      ) : isCorrect ? (
+        <div className="iscorrect">
+          <img className="iscorrect__img" src={right} alt="right" />
+        </div>
+      ) : (
+        <div className="iscorrect">
+          <img className="iscorrect__img" src={wrong} alt="wrong" />
+        </div>
       )}
-      <p className="question-box__question">
-        {!hasGameStarted && !hasGameEnded
-          ? "Welcome to Mouse Fan Trivia! How many questions can you answer?"
-          : hasGameEnded ? "Game Over! Click on Start Game to try again!" : questionData}
-      </p>
     </div>
   );
 }
