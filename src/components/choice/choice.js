abbf9d8279cb9ce78f30ed5971d32651a5aae873
William Chrapcynski <wchrapcynski@gmail.com>
2020-06-01 13:59:15 -0400

    working on responsiveness

--- src/components/choice/choice.js
+++ src/components/choice/choice.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as gameActions from "./../../actions/gameActions";
 import "./choice.scss";
@@ -8,11 +8,9 @@ function Choice(props) {
   const { hasGameStarted, isQuestionActive, score, highScore } = useSelector(
     (state) => state.gameReducer
   );
-  const [clicked, setClicked] = useState(false);
 
   const clickHandle = () => {
     if (isQuestionActive) {
-      setClicked(true);
       if (props.correct) {
         dispatch(gameActions.updateIsCorrect(true));
         dispatch(gameActions.updateScore(score + 1));
@@ -30,15 +28,9 @@ function Choice(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [score]);
 
-  useEffect(() => {
-    if (isQuestionActive === true) {
-      setClicked(false);
-    }
-  }, [isQuestionActive]);
-
   return (
     <div className="choice" onClick={clickHandle}>
-      <p className={!isQuestionActive && !clicked ? "choice-disable" : "choice-active"}>
+      <p className={!isQuestionActive ? "choice-disable" : "choice-active"}>
         {hasGameStarted && props.choice}
       </p>
     </div>
