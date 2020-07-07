14c20c54539dfa47cd9a0cfa6b0fd6602be62a36
William Chrapcynski <wchrapcynski@gmail.com>
2020-07-07 15:51:33 -0400

    Set up answers module for testing

--- src/components/answers/answers.js
+++ src/components/answers/answers.js
@@ -8,30 +8,38 @@ function Answers(props) {
   const { choices } = props;
 
   return (
-    <div className="answers">
-      <Choice
-        choice={choices && choices[0].choice}
-        correct={choices && choices[0].correct}
-      />
-      <Choice
-        choice={choices && choices[1].choice}
-        correct={choices && choices[1].correct}
-      />
-      <Choice
-        choice={choices && choices[2].choice}
-        correct={choices && choices[2].correct}
-      />
-      <Choice
-        choice={choices && choices[3].choice}
-        correct={choices && choices[3].correct}
-      />
+    <div className="answers" data-test="answers-component">
+      <div data-test="choice">
+        <Choice
+          choice={choices && choices[0].choice}
+          correct={choices && choices[0].correct}
+        />
+      </div>
+      <div data-test="choice">
+        <Choice
+          choice={choices && choices[1].choice}
+          correct={choices && choices[1].correct}
+        />
+      </div>
+      <div data-test="choice">
+        <Choice
+          choice={choices && choices[2].choice}
+          correct={choices && choices[2].correct}
+        />
+      </div>
+      <div data-test="choice">
+        <Choice
+          choice={choices && choices[3].choice}
+          correct={choices && choices[3].correct}
+        />
+      </div>
     </div>
   );
 }
 
 Answers.propTypes = {
-  choices: PropTypes.arrayOf(PropTypes.object)
-}
+  choices: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default connect((state) => ({
   choices: state.questionReducer.choices,
