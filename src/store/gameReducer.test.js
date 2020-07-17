cc1947435e3c4600e12d1abcb5b11c52d584e58a
William Chrapcynski <wchrapcynski@gmail.com>
2020-07-17 19:38:03 -0400

    Updated gameReducer test

--- src/store/gameReducer.test.js
+++ src/store/gameReducer.test.js
@@ -6,4 +6,15 @@ describe("Game Reducer", () => {
     const newState = gameReducer(undefined, {});
     expect(newState).toEqual(initialState);
   });
+  it("should return a new state if receiving type", () => {
+    const gameData = true;
+    const newState = gameReducer(undefined, {
+      type: types.UDPATE_HAS_GAME_STARTED,
+      payload: gameData,
+    });
+    expect(newState).toEqual({
+      ...initialState,
+      hasGameStarted: gameData,
+    });
+  });
 });
