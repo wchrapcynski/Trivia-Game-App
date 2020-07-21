aba8e2825a6bbff90af72bafa4652c495de45ede
William Chrapcynski <wchrapcynski@gmail.com>
2020-07-20 21:02:39 -0400

    Created integrated test for fetching of question data

--- src/integrationTests/index.test.js
+++ src/integrationTests/index.test.js
@@ -1,8 +1,9 @@
 import moxios from "moxios";
 import { testStore } from "./../helpers/utils";
 import { fetchLeaderboardData } from "./../actions/leaderboardActions";
+import { fetchQuestionData } from "./../actions/questionActions";
 
-describe("Fetch leaderboard actions", () => {
+describe("Fetch leaderboard action", () => {
   beforeEach(() => {
     moxios.install();
   });
@@ -41,6 +42,48 @@ describe("Fetch leaderboard actions", () => {
         const newState = store.getState();
         expect(newState.leaderboardReducer.leaderboardData).toStrictEqual(
           expectedState
+        );
+      });
+  });
+});
+
+describe("fetch question action", () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it("should update correctly", () => {
+    const expectedState = {
+      question: "Test Question",
+      choice1: "Test Choice1",
+      choice2: "Test Choice2",
+      choice3: "Test Choice3",
+      choice4: "Test Choice4",
+    };
+    const store = testStore();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({ status: 200, response: expectedState });
+    });
+
+    const data = store.getState();
+
+    return store
+      .dispatch(
+        fetchQuestionData(
+          data.gameReducer.apiEndPoints.leaderboard,
+          data.gameReducer.apiAccessOptions,
+          "1"
+        )
+      )
+      .then(() => {
+        const newState = store.getState();
+        expect(newState.questionReducer.question).toStrictEqual(
+          expectedState.question
         );
       });
   });
